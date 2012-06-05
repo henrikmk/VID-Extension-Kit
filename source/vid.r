cbb6666e324cbf7e5a44382efb82ac9d0c5ab460
Henrik Mikael Kristensen <henrikmk@gmail.com>
2012-06-05 17:16:42 +0200

    - adds ON-REFRESH actor - changes header to 2008 RT header

--- source/vid.r
+++ source/vid.r
@@ -1,16 +1,19 @@
 REBOL [
 	Title:  "REBOL/View: Visual Interface Dialect"
-	Author: "Carl Sassenrath"
-	Rights: "Copyright 2000 REBOL Technologies. All rights reserved."
-	; You are free to use, modify, and distribute this software with any
-	; REBOL Technologies products as long as the above header, copyright,
-	; and this comment remain intact. This software is provided "as is"
-	; and without warranties of any kind. In no event shall the owners or
-	; contributors be liable for any damages of any kind, even if advised
-	; of the possibility of such damage. See license for more information.
+	Version: 2.7.6
+	Rights: "Copyright REBOL Technologies 2008. All rights reserved."
+	Home: http://www.rebol.com
+	Date: 14-Mar-2008
+
+	; You are free to use, modify, and distribute this file as long as the
+	; above header, copyright, and this entire comment remains intact.
+	; This software is provided "as is" without warranties of any kind.
+	; In no event shall REBOL Technologies or source contributors be liable
+	; for any damages of any kind, even if advised of the possibility of such
+	; damage. See license for more information.
 
 	; Please help us to improve this software by contributing changes and
-	; fixes via http://www.rebol.com/feedback.html - Thanks!
+	; fixes. See http://www.rebol.com/support.html for details.
 ]
 
 find-key-face: func [
@@ -238,6 +241,7 @@ vid-face/actors: make object! [
 	on-minimize:
 	on-restore:
 	on-text:
+	on-refresh:
 	on-offset:
 		none
 ]
@@ -406,6 +410,7 @@ facet-words: [ ; Order of these is important to the code
 	on-minimize			[set-actor new 'on-minimize second args next args]
 	on-restore			[set-actor new 'on-restore second args next args]
 	on-text				[set-actor new 'on-text second args next args]
+	on-refresh			[set-actor new 'on-refresh second args next args]
 	on-offset			[set-actor new 'on-offset second args next args]
 ]
 
